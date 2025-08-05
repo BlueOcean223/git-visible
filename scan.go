fcaf4c2a1830f710eb0451a96b56e17c3c824d95
BlueOcean223 <1751259208@qq.com>
2025-08-05 22:50:16 +0800

    优化添加仓库时的提示

--- scan.go
+++ scan.go
@@ -37,20 +37,21 @@ func parseFileLinesToSlice(filePath string) []string {
 	return strings.Split(strings.TrimSpace(string(content)), "\n")
 }
 
-// joinSlices 将新增的文件路径添加到已有的 slice 中，已有文件路径的不重复添加
-func joinSlices(new []string, existing []string) []string {
+// getNewRepos 获取新增的仓库路径
+func getNewRepos(new, existing []string) []string {
 	hash := make(map[string]bool, len(existing))
 	for _, path := range existing {
 		hash[path] = true
 	}
 
+	var newRepos []string
 	for _, i := range new {
 		if !hash[i] {
-			existing = append(existing, i)
+			newRepos = append(newRepos, i)
 			hash[i] = true
 		}
 	}
-	return existing
+	return newRepos
 }
 
 // dumpStringsSliceToFile 将 slice 内容写入文件
@@ -65,7 +66,26 @@ func dumpStringsSliceToFile(repos []string, filePath string) {
 // addNewSliceElementsToFile 添加新的 slice 元素到文件
 func addNewSliceElementsToFile(filePath string, newRepos []string) {
 	existingRepos := parseFileLinesToSlice(filePath)
-	repos := joinSlices(newRepos, existingRepos)
+	// 向用户展示已有的仓库路径
+	if len(existingRepos) > 0 {
+		fmt.Println("已有的git仓库路径:")
+		for _, repo := range existingRepos {
+			fmt.Println(repo)
+		}
+		fmt.Printf("\n")
+	}
+	// 获取新增仓库路径，并展示
+	newRepos = getNewRepos(newRepos, existingRepos)
+	if len(newRepos) > 0 {
+		fmt.Println("新增的git仓库路径:")
+		for _, repo := range newRepos {
+			fmt.Println(repo)
+		}
+	} else {
+		fmt.Println("没有新增的git仓库路径")
+	}
+	// 合并仓库路径
+	repos := append(existingRepos, newRepos...)
 	dumpStringsSliceToFile(repos, filePath)
 }
 
@@ -81,7 +101,6 @@ func scan(folder string) {
 
 	filePath := getDotFilePath()
 	addNewSliceElementsToFile(filePath, repositories)
-	fmt.Printf("\n添加git仓库成功\n")
 }
 
 // scanGitFolders 返回包含 .git 文件夹的父文件夹路径
@@ -98,7 +117,6 @@ func scanGitFolders(folder string) []string {
 			if name == ".git" {
 				// 获取 .git 文件夹的父文件夹路径
 				repoPath := filepath.Dir(path)
-				fmt.Println(repoPath)
 				folders = append(folders, repoPath)
 				// 跳过当前文件夹，即不遍历其子目录
 				return filepath.SkipDir
